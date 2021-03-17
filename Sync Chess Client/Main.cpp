e59404a3cece1cc005fe0c47471908ef4e89d08e
DarnYo <nedelcuandrei64@gmail.com>
2021-03-17 16:41:38 +0200

    Add project files.

---
+++ Sync Chess Client/Main.cpp
@@ -0,0 +1,87 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif // !WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <cassert>
+#include <cstdio>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	WSADATA wsaData;
+	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	assert(result == 0);
+
+	const addrinfo hints = {
+		.ai_family = AF_INET,
+		.ai_socktype = SOCK_STREAM,
+		.ai_protocol = IPPROTO_TCP
+	};
+
+	// Resolve server address and port
+	addrinfo* info;
+	result = getaddrinfo("localhost", "8888", &hints, &info);
+	if (result != 0)
+	{
+		printf("getaddrinfo() failed: %d\n", result);
+		WSACleanup();
+		return -1;
+	}
+
+	// Create socket
+	SOCKET connect_socket = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+	if (connect_socket == INVALID_SOCKET)
+	{
+		printf("Unable to create server socket: %d\n", WSAGetLastError());
+		freeaddrinfo(info);
+		WSACleanup();
+		return -1;
+	}
+
+	result = connect(connect_socket, info->ai_addr, (int)info->ai_addrlen);
+	freeaddrinfo(info);
+	if (connect_socket == SOCKET_ERROR)
+	{
+		printf("Unable to connect to server.\n");
+		closesocket(connect_socket);
+		WSACleanup();
+		return -1;
+	}
+
+	const int buffer_size = 512;
+	char buffer[buffer_size];
+	strcpy_s(buffer, buffer_size, "Hello server");
+
+	do
+	{
+		// Send to server
+		result = send(connect_socket, buffer, strlen(buffer) + 1, 0);
+		if (result == SOCKET_ERROR)
+		{
+			printf("send() failed with error: %d\n", WSAGetLastError());
+		}
+
+		// Receive from server
+		result = recv(connect_socket, buffer, buffer_size, 0);
+		if (result > 0)
+		{
+			printf("Received %d bytes: %s\n", strlen(buffer), buffer);
+		}
+		else if (result == 0)
+		{
+			printf("Connection to server closing...\n");
+		}
+		else
+		{
+			printf("recv() failed with error: %d\n", WSAGetLastError());
+		}
+	} while (result > 0);
+
+	closesocket(connect_socket);
+	WSACleanup();
+	return 0;
+}
---
+++ Sync Chess Server/Main.cpp
@@ -0,0 +1,108 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif // !WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <cassert>
+#include <cstdio>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	WSADATA wsaData;
+	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	assert(result == 0);
+
+	const addrinfo hints = {
+		.ai_flags = AI_PASSIVE,
+		.ai_family = AF_INET,
+		.ai_socktype = SOCK_STREAM,
+		.ai_protocol = IPPROTO_TCP
+	};
+
+	// Resolve serevr local address and port
+	addrinfo* info;
+	result = getaddrinfo(nullptr, "8888", &hints, &info);
+	if (result != 0)
+	{
+		printf("getaddrinfo() failed: %d\n", result);
+		WSACleanup();
+		return -1;
+	}
+
+	// Create server socket
+	SOCKET listen_socket = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+	if (listen_socket == INVALID_SOCKET)
+	{
+		printf("Unable to create server socket: %d\n", WSAGetLastError());
+		freeaddrinfo(info);
+		WSACleanup();
+		return -1;
+	}
+
+	// Setup TCP listening socket
+	result = bind(listen_socket, info->ai_addr, (int)info->ai_addrlen);
+	if (result == SOCKET_ERROR)
+	{
+		printf("bind() failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(info);
+		closesocket(listen_socket);
+		WSACleanup();
+		return -1;
+	}
+
+	// Listen for a single connection
+	result = listen(listen_socket, 1);
+	if (result == SOCKET_ERROR)
+	{
+		printf("bind() failed with error: %d\n", WSAGetLastError());
+		closesocket(listen_socket);
+		WSACleanup();
+		return -1;
+	}
+
+	// Accept a client socket
+	SOCKET client_socket = accept(listen_socket, nullptr, nullptr);
+	if (client_socket == INVALID_SOCKET)
+	{
+		printf("accept() failed with errror: %d\n", WSAGetLastError());
+		closesocket(listen_socket);
+		WSACleanup();
+		return -1;
+	}
+
+	const int buffer_size = 512;
+	char buffer[buffer_size];
+
+	do
+	{
+		// Wait for client to send some data
+		result = recv(client_socket, buffer, buffer_size, 0);
+		if (result > 0)
+		{
+			printf("Received %d bytes: %s\n", strlen(buffer), buffer);
+
+			// Send back to client
+			result = send(client_socket, buffer, result, 0);
+			if (result == SOCKET_ERROR)
+			{
+				printf("send() failed with error: %d\n", WSAGetLastError());
+			}
+		}
+		else if (result == 0)
+		{
+			printf("Connection to client closing...\n");
+		}
+		else
+		{
+			printf("recv() failed with error: %d\n", WSAGetLastError());
+		}
+	} while (result > 0);
+
+	closesocket(listen_socket);
+	WSACleanup();
+	return 0;
+}
